3ce13ce2766a1bc0a96ec1106e1b8f4658c06784
Caleb Evans <caleb@calebevans.me>
2017-05-09 16:28:04 -0700

    Add mocha/chai and some initial unit tests

---
+++ test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const expect = require('chai').expect;
+const Plugin = require('.');
+
+describe('rsvg-brunch', function () {
+
+  // Brunch will automatically supply the default public directory path if it is
+  // not explicitly overridden by the user
+  const defaultConfig = {paths: {public: 'public'}};
+
+  it('should initialize with empty brunch config', function () {
+    const plugin = new Plugin(defaultConfig);
+    expect(plugin).to.be.ok;
+  });
+
+  it('should initialize with empty plugins config', function () {
+    const plugin = new Plugin(
+      Object.assign({}, defaultConfig, {plugins: {}}));
+    expect(plugin).to.be.ok;
+  });
+
+  it('should initialize with empty plugin config', function () {
+    const plugin = new Plugin(
+        Object.assign({}, defaultConfig, {plugins: {rsvg: {}}}));
+    expect(plugin).to.be.ok;
+  });
+
+});
