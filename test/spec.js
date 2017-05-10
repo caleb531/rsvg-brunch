6a91643d15f0b5ab67e0744c4b907b35df363ee3
Caleb Evans <caleb@calebevans.me>
2017-05-10 16:14:04 -0700

    Refactor mock test input into beforeEach

--- test/spec.js
+++ test/spec.js
@@ -47,152 +47,136 @@ describe('rsvg-brunch', function () {
     sinon.assert.calledOnce(loggerWarnSpy);
   });
 
-  it('should apply global output defaults', function () {
-    const conversion = {
-      input: 'input.svg',
-      output: [{width: 100, path: 'output.png'}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('format', 'png');
-  });
+  describe('extendOutputProps', function () {
 
-  it('should apply conversion output defaults', function () {
-    const conversion = {
-      input: 'input.svg',
-      outputDefaults: {format: 'abc', path: 'output.abc'},
-      output: [{width: 100}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('format', 'abc');
-  });
-
-  it('should allow output file to override defaults', function () {
-    const conversion = {
-      input: 'input.svg',
-      outputDefaults: {format: 'abc'},
-      output: [{width: 100, format: 'def', path: 'output.def'}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('format', 'def');
-  });
-
-  it('should prepend public directory to output file path', function () {
-    const conversion = {
-      input: 'input.svg',
-      output: [{width: 100, path: 'output.png'}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('path', 'public/output.png');
-  });
-
-  it('should supply output file height if missing', function () {
-    const conversion = {
-      input: 'input.svg',
-      output: [{width: 100, path: 'output.png'}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('height', 100);
-  });
-
-  it('should supply output file width if missing', function () {
-    const conversion = {
-      input: 'input.svg',
-      output: [{height: 100, path: 'output.png'}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('width', 100);
-  });
-
-  it('should evaluate {width} path variable', function () {
-    const conversion = {
-      input: 'input.svg',
-      outputDefaults: {path: 'output-{width}.png'},
-      output: [{width: 200, height: 100}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('path', 'public/output-200.png');
-  });
-
-  it('should evaluate {height} path variable', function () {
-    const conversion = {
-      input: 'input.svg',
-      outputDefaults: {path: 'output-{height}.png'},
-      output: [{width: 200, height: 100}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('path', 'public/output-100.png');
-  });
-
-  it('should evaluate {format} path variable', function () {
-    const conversion = {
-      input: 'input.svg',
-      outputDefaults: {path: 'output.{format}'},
-      output: [{width: 200, height: 100, format: 'abc'}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('path', 'public/output.abc');
-  });
-
-  it('should evaluate {id} path variable', function () {
-    const conversion = {
-      input: 'input.svg',
-      outputDefaults: {path: 'output-{id}.png'},
-      output: [{width: 200, height: 100, id: 'foo'}]
-    };
-    const plugin = new Plugin(defaultConfig);
-    expect(plugin.extendOutputProps(conversion, conversion.output[0]))
-      .to.have.property('path', 'public/output-foo.png');
-  });
-
-  it('should resolve conversion with correct parameters', function (done) {
-    const outputFile = {
-      format: 'png',
-      width: 200,
-      height: 100,
-      path: path.join(
-        tmp.dirSync().name,
-        tmp.tmpNameSync({template: 'XXXXXX.png'})
-      )
-    };
-    const plugin = new Plugin(defaultConfig);
-    const outputPromise = plugin.convertSvg('test/input.svg', outputFile);
-    outputPromise.then(() => {
-      expect(fs.existsSync(outputFile.path)).to.be.true;
-      fs.unlinkSync(outputFile.path);
-      done();
-    }).catch(() => {
-      done(new Error('expected promise to resolve, but rejected instead'));
+    let conversion;
+    beforeEach(function () {
+      conversion = {
+        input: 'input.svg',
+        outputDefaults: {},
+        output: [{width: 200, height: 100}]
+      };
     });
+
+    it('should apply global output defaults', function () {
+      conversion.output[0].path = 'output.png';
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('format', 'png');
+    });
+
+    it('should apply conversion output defaults', function () {
+      conversion.outputDefaults.format = 'abc';
+      conversion.outputDefaults.path = 'output.abc';
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('format', 'abc');
+    });
+
+    it('should allow output file to override defaults', function () {
+      conversion.outputDefaults.format = 'abc';
+      conversion.output[0].format = 'def';
+      conversion.output[0].path = 'output.def';
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('format', 'def');
+    });
+
+    it('should prepend public directory to output file path', function () {
+      conversion.output[0].path = 'output.png';
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('path', 'public/output.png');
+    });
+
+    it('should supply output file height if missing', function () {
+      conversion.output[0].path = 'output.png';
+      delete conversion.output[0].height;
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('height', 200);
+    });
+
+    it('should supply output file width if missing', function () {
+      conversion.output[0].path = 'output.png';
+      delete conversion.output[0].width;
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('width', 100);
+    });
+
+    it('should evaluate {width} path variable', function () {
+      conversion.outputDefaults.path = 'output-{width}.png';
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('path', 'public/output-200.png');
+    });
+
+    it('should evaluate {height} path variable', function () {
+      conversion.outputDefaults.path = 'output-{height}.png';
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('path', 'public/output-100.png');
+    });
+
+    it('should evaluate {format} path variable', function () {
+      conversion.outputDefaults.path = 'output.{format}';
+      conversion.output[0].format = 'abc';
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('path', 'public/output.abc');
+    });
+
+    it('should evaluate {id} path variable', function () {
+      conversion.outputDefaults.path = 'output-{id}.png';
+      conversion.output[0].id = 'foo';
+      const plugin = new Plugin(defaultConfig);
+      expect(plugin.extendOutputProps(conversion, conversion.output[0]))
+        .to.have.property('path', 'public/output-foo.png');
+    });
+
   });
 
-  it('should reject conversion with invalid parameters', function (done) {
-    const outputFile = {
-      format: 'png',
+  describe('convertSvg', function () {
+
+    let outputFile;
+    beforeEach(function () {
+      outputFile = {
+        format: 'png',
+        width: 200,
+        height: 100,
+        path: path.join(
+          tmp.dirSync().name,
+          tmp.tmpNameSync({template: 'XXXXXX.png'})
+        )
+      };
+    });
+
+    it('should resolve with correct params', function (done) {
+      const plugin = new Plugin(defaultConfig);
+      const outputPromise = plugin.convertSvg('test/input.svg', outputFile);
+      outputPromise.then(() => {
+        expect(fs.existsSync(outputFile.path)).to.be.true;
+        fs.unlinkSync(outputFile.path);
+        done();
+      }).catch(() => {
+        done(new Error('expected promise to resolve, but rejected instead'));
+      });
+    });
+
+    it('should reject with invalid params', function (done) {
       // width cannot be zero; will cause librsvg to throw an error
-      width: 0,
-      height: 100,
-      path: path.join(
-        tmp.dirSync().name,
-        tmp.tmpNameSync({template: 'XXXXXX.png'})
-      )
-    };
-    const plugin = new Plugin(defaultConfig);
-    const outputPromise = plugin.convertSvg('test/input.svg', outputFile);
-    outputPromise.then(() => {
-      done(new Error('expected promise to reject, but resolved instead'));
-    }).catch(() => {
-      expect(fs.existsSync(outputFile.path)).to.be.false;
-      done();
+      outputFile.width = 0;
+      const plugin = new Plugin(defaultConfig);
+      const outputPromise = plugin.convertSvg('test/input.svg', outputFile);
+      outputPromise.then(() => {
+        done(new Error('expected promise to reject, but resolved instead'));
+      }).catch(() => {
+        expect(fs.existsSync(outputFile.path)).to.be.false;
+        done();
+      });
     });
+
   });
 
   it('should be registered as Brunch plugin', function () {
