7abd7279358192d8cbfc10e472828241f0e9d2dd
Caleb Evans <caleb@calebevans.me>
2017-05-09 17:04:13 -0700

    Add tests for requiring librsvg

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const expect = require('chai').expect;
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+const logger = require('loggy');
 const Plugin = require('.');
 
 describe('rsvg-brunch', function () {
@@ -24,6 +27,25 @@ describe('rsvg-brunch', function () {
     const plugin = new Plugin(
         Object.assign({}, defaultConfig, {plugins: {rsvg: {}}}));
     expect(plugin).to.be.ok;
+  });
+
+  it('should require Rsvg module if installed', function () {
+    const plugin = new Plugin(defaultConfig);
+    expect(plugin).to.have.property('Rsvg');
+  });
+
+  it('should catch error if system librsvg is not installed', function () {
+    const origLoggerWarn = logger.warn;
+    logger.warn = sinon.spy();
+    try {
+      // Cause require('librsvg').Rsvg to throw an error
+      let ProxiedPlugin = proxyquire('.', {librsvg: null});
+      const plugin = new ProxiedPlugin(defaultConfig);
+      expect(plugin).not.to.have.property('Rsvg');
+      sinon.assert.calledOnce(logger.warn);
+    } finally {
+      logger.warn = origLoggerWarn;
+    }
   });
 
 });
